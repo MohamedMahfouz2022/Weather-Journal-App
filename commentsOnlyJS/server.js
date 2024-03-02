9e378aa373f894e4f0690d3c92695879ce0dda19
Mohamed_Mahfouz <98268832+MohamedMahfouz2022@users.noreply.github.com>
2024-03-02 16:13:51 +0200

    Update server.js

--- commentsOnlyJS/server.js
+++ commentsOnlyJS/server.js
@@ -1,22 +1,39 @@
 // Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
+projectData = {};
 
+// Require Express to run server and routes
+const cors = require('cors')
+const express = require('express')
+const bodyParser = require('body-parser');
 // Start up an instance of app
+const app = express()
 
-/* Dependencies */
 /* Middleware*/
-
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
-
+app.use(cors())
 // Initialize the main project folder
+app.use(express.static('website'));
+//prot
+const port = 4800
+// Setup Server
+app.listen(port, () => {
+    console.log(`Server Running On: http://localhost:${port}}`)
+})
 
-// Spin up the server
-// Callback to debug
-
-// Initialize all route with a callback function
-
-// Callback function to complete GET '/all'
-
-// Post Route
-  
+// =---------------------------------------------------
+//Callback Function Get all
+const getAll = (req, res) => res.send(projectData).status(200);
+//Get
+app.get("/all", getAll)
+//----PostData---=>
+const postData = (req, res) => {
+    projectData = req.body
+    res.send(projectData).status(200)
+    console.log(projectData)
+}
+//Post
+app.post('/add', postData)
